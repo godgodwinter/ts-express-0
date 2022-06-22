5a833a6b59dccefc9b1af117c80a7bebee658df4
godgodwinter <kakadz.pro@gmail.com>
2022-06-23 00:54:35 +0700

    Update settings and build

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
 import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser"; //untuk menampilkan data request berbentuk json
+import morgan from "morgan"; //untukk logger / melihat riwayat yang di konsumsi API
+
 
 class App {
     public app: Application
     constructor() {
         this.app = express();
+        this.plugins();
         this.routes();
+    }
+
+    protected plugins(): void {
+        this.app.use(bodyParser.json());
+        this.app.use(morgan("dev"));
     }
 
     protected routes(): void {
         this.app.route("/").get((req: Request, res: Response) => {
             res.send('Ini Express dengan TS');
+        })
+
+        this.app.route("/users").post((req: Request, res: Response) => {
+            res.send(req.body);
         })
     }
 }
