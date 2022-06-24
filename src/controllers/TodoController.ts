ecf3359a772fc928e8b5b48b039871429bd04730
godgodwinter <kakadz.pro@gmail.com>
2022-06-24 11:52:42 +0700

    add edit and update todo

--- src/controllers/TodoController.ts
+++ src/controllers/TodoController.ts
@@ -4,8 +4,17 @@ const db = require("../db/models")
 
 
 class TodoController implements IController {
-    index(req: Request, res: Response): Response {
-        return res.send("index")
+    index = async (req: Request, res: Response): Promise<Response> => {
+        const { id } = req.app.locals.credential;
+        const todos = await db.todo.findAll({
+            where: { user_id: id },
+            attributes: ['id', 'desc']
+        });
+
+        return res.send({
+            data: todos,
+            message: "Success"
+        });
     }
     create = async (req: Request, res: Response): Promise<Response> => {
         const { id } = req.app.locals.credential;
@@ -19,16 +28,46 @@ class TodoController implements IController {
             message: "Create Success"
         });
     }
-    show(req: Request, res: Response): Response {
-        return res.send("show");
+    show = async (req: Request, res: Response): Promise<Response> => {
+        const { id: user_id } = req.app.locals.credential; //varibale id dari app local(user login ) direname jadi user_id
+        const { id } = req.params; // id yang ini dari id params
+
+        const todo = await db.todo.findOne({
+            where: { id, user_id }
+        })
+        return res.send({
+            data: todo,
+            message: "Get Data Success"
+        });
     }
-    update(req: Request, res: Response): Response {
-        return res.send("Update Success")
+    update = async (req: Request, res: Response): Promise<Response> => {
+        const { id: user_id } = req.app.locals.credential; //varibale id dari app local(user login ) direname jadi user_id
+        const { id } = req.params; // id yang ini dari id params
+        const { desc } = req.body; // id yang ini dari id params
+
+        await db.todo.update({
+            desc
+        }, {
+            where: {
+                id, user_id
+            }
+        })
+
+        return res.send({
+            data: "",
+            message: "Update Success"
+        });
+    }
+
+    delete = async (req: Request, res: Response): Promise<Response> => {
+        const { id: user_id } = req.app.locals.credential; //varibale id dari app local(user login ) direname jadi user_id
+        const { id } = req.params; // id yang ini dari id params
 
 
-    }
-    delete(req: Request, res: Response): Response {
-        return res.send("delete");
+        return res.send({
+            data: "",
+            message: "Success"
+        });
     }
 
 }
